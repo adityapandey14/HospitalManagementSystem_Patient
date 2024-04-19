6f7dbcf13d1b4ab61acf7e6ac0e65f3abf3fbd03
Aditya Pandey <anshu61796@gmail.com>
2024-04-19 14:33:34 +0530

    Initial Commit

---
+++ Patient_HMS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Patient_HMS
+//
+//  Created by Aditya Pandey on 19/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Patient_HMS/Patient_HMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Patient_HMSApp.swift
+//  Patient_HMS
+//
+//  Created by Aditya Pandey on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Patient_HMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
